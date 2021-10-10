f7d53a7375abe6b448c1a3ff38a2b4ce6167c8ba
Ali Omar <ali.omar@umbrellanetworks.net>
2021-10-10 14:29:05 +0300

    installed apps typo corrected and saved!

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_authtoken',
+    'rest_framework.authtoken',
     'profiles_api',
 ]
 
