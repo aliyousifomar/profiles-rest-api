49072bd67327cc2bc7432ffccc600e9b096a1e4c
Ali Omar <ali.omar@umbrellanetworks.net>
2021-12-06 19:43:31 +0300

    custom models added

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
